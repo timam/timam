84b5f458ecee676f7146a2654053de0042f8c0f4
Timam <mail@timam.io>
2021-04-11 21:33:28 +0600

    added our own middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/timam/timam/pkg/config"
 	"github.com/timam/timam/pkg/handlers"
 	"net/http"
@@ -9,6 +10,10 @@ import (
 
 func routes(app *config.AppConfig)  http.Handler{
 	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(WriteToConsole)
+
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 	return mux
