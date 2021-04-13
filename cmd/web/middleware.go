0264683497b86c75bd480158a13f12e7e9989a8f
Timam <mail@timam.io>
2021-04-14 04:32:54 +0600

    session init

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/justinas/nosurf"
 	"net/http"
 )
 
@@ -12,4 +13,16 @@ func WriteToConsole(next http.Handler) http.Handler  {
 		next.ServeHTTP(w, r)
 	})
 
+}
+
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path: "/",
+		Secure: false,
+		SameSite: http.SameSiteLaxMode,
+	})
+
+	return csrfHandler
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/timam/timam
 
 go 1.15
 
-require github.com/go-chi/chi/v5 v5.0.2
+require (
+	github.com/alexedwards/scs/v2 v2.4.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.2
+	github.com/justinas/nosurf v1.1.1 // indirect
+)
